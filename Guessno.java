5ee72c6e814466db6221b85bdcd3da2210205bce
21rr10 <ranjan.ritesh21102003@gmail.com>
2023-11-02 12:55:17 +0530

    Initial commit - Number Guessing Game in Java
    This commit includes the initial version of a simple Number Guessing Game implemented in Java. The game generates a random number within a specified range and allows players to make guesses with real-time feedback. It's an excellent project for beginners to practice Java programming fundamentals.

---
+++ Guessno.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Guessno {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = (int) (Math.random() * 100);
+        int guess = 0, attempt = 0;
+        while (num >= 0) {
+            System.out.println("Guess my number");
+            guess = sc.nextInt();
+
+            if (num == guess) {
+                ++attempt;
+
+                break;
+            } else if (num > guess) {
+                System.out.println("Your number is bigger than " + guess);
+                ++attempt;
+
+            } else {
+                System.out.println("Your number is smaller than " + guess);
+                ++attempt;
+            }
+        }
+        System.out.println("My number was " + num);
+        System.out.println("Your number is " + guess);
+        System.out.println("You have guessed correctly and in " + attempt + "th attempt");
+
+    }
+
+}
